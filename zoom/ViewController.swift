b4703aae7e1be83075be861a3b97d169bf1304e5
andyhaz <santelife@gmail.com>
2021-02-08 13:03:23 -0600

    Initial Commit

---
+++ zoom/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  zoom
+//
+//  Created by andrew hazlett on 2/8/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
